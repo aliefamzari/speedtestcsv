4ea8ad3138c90402ff3e5a1da3d8505a3fc47e25
aliefamzari <aliefamzari@gmail.com>
2021-06-09 18:42:44 +0800

    changetospeedtestookla

--- speedtestcsv.sh
+++ speedtestcsv.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 #Check command
-commandlist=("speedtest-cli" "wget" "curl")
-for i in "${commandlist[@]}"; do
-    if ! [ -x "$(command -v $i)" ]; then
-        echo "$i not exist" 
-        exit 1
-    fi
-    done
+# commandlist=("/home/pi/speedtestookla/speedtest" "wget" "curl")
+# for i in "${commandlist[@]}"; do
+#     if ! [ -x "$(command -v $i)" ]; then
+#         echo "$i not exist" 
+#         exit 1
+#     fi
+#     done
 
 
 
 
-date=$(date +"%Y%m%d_%H%M")
-header1=$(speedtest-cli --csv-header)
-result=$(speedtest-cli --share --csv)
-isp=$(curl -s ipinfo.io/org)
-
+timestamp=$(date --iso-8601=s)
+header1="Server_name,Server_id,Latency,Jitter,Packet_loss,Download,Upload,Download_bytes,Upload_bytes,Share_url"
 #Check CSV file if exist, and write CSV file if not exist.
 if ! [ -f result.csv ]; then
-    echo "ISP",$header1 > result.csv
-    #Check internet
-    wget -q --spider http://google.com
-    if ! [ $? -eq 0 ]; then
-    echo "OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE" >> result.csv
-        else
-        echo \"$isp\",$result >> result.csv
-    
-    fi
-
+    echo "Time", "ISP",$header1 > result.csv
 fi
+
+#Check internet
+wget -q --spider http://google.com
+internet=$(echo $?)
+if [ $internet != 0 ]; then
+    echo "$timestamp","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE","OFFLINE" >> result.csv
+    else 
+        isp=$(curl -s ipinfo.io/org)
+        result=$(/home/pi/speedtestookla/speedtest -f csv)
+        echo \"$isp\",$result >> result.csv
+fi  
+        
+
+
 
 
