aef19834b378d2b1adf51b54ea63389fbcddf652
aliefamzari <aliefamzari@gmail.com>
2021-06-11 17:38:01 +0800

    rear

--- speedtestcsv.sh
+++ speedtestcsv.sh
@@ -5,6 +5,11 @@
 
 #Speedtest bin path
 speedtest="/home/pi/speedtestookla/speedtest"  #PLEASE CHANGE THIS PATH POINT TO WHERE YOU SAVE YOUR SPEEDTEST INSTALL. 
+#Get current directory where script is located. Result will be stored on same directory as script file. 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+timestamp=$(date --iso-8601=s)
+#CSV file header
+header1="Server_name,Server_id,Latency,Jitter,Packet_loss,Download,Upload,Download_bytes,Upload_bytes,Share_url"
 
 # Check command
 commandlist=("$speedtest" "wget" "curl")
@@ -15,11 +20,7 @@ for i in "${commandlist[@]}"; do
     fi
 done
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-
-timestamp=$(date --iso-8601=s)
-header1="Server_name,Server_id,Latency,Jitter,Packet_loss,Download,Upload,Download_bytes,Upload_bytes,Share_url"
 #Check CSV file if exist, and write CSV file if not exist.
 if ! [ -f $SCRIPT_DIR/result.csv ]; then
     echo "Time", "ISP",$header1 > $SCRIPT_DIR/result.csv
