ed3b234fe0353282f956bd4162571e182161fbeb
aliefamzari <aliefamzari@gmail.com>
2021-06-12 20:23:19 +0800

    1mt

--- speedtestcsv.sh
+++ speedtestcsv.sh
@@ -31,13 +31,13 @@ if ! [ -f $SCRIPT_DIR/result.csv ]; then
 fi
 
 #TODO - Keep only 1 month CSV row
-mtd=$(date --iso-8601 |cut -d '-' -f2) 
-csvmnt=$(sed -n '2p' $SCRIPT_DIR/result.csv |cut -d- -f2)
+# mtd=$(date --iso-8601 |cut -d '-' -f2) 
+# csvmnt=$(sed -n '2p' $SCRIPT_DIR/result.csv |cut -d- -f2)
 
-if ! [ $mtd = $csvmnt ]; then
-    rm $SCRIPT_DIR/result.csv
-    header
-fi
+# if ! [ $mtd = $csvmnt ]; then
+#     rm $SCRIPT_DIR/result.csv
+#     header
+# fi
 
 #Check internet if offline or online
 wget -q --spider http://google.com
